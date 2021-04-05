3e376c0e073761eb960dd113f56514ed4a814c99
sev-30 <75568227+sev-30@users.noreply.github.com>
2021-04-05 10:40:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
-// index.php
-
 
 require_once 'Bicycle.php';
+require_once 'Car.php';
 
 $bike = new Bicycle('blue', 15, 1, 2);
 // var_dump($bike);
@@ -15,4 +14,14 @@ echo $bike->forward();
 echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
 echo $bike->brake();
 echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
-echo $bike->brake(); 
+echo $bike->brake();
+
+$car = new Car('yellow', 5, 'gasoline');
+
+// Moving car
+
+echo $car->forward();
+echo '<br> Vitesse de la voiture : ' . $car->getCurrentSpeed() . ' km/h' . '<br>';
+echo $car->brake();
+echo '<br> Vitesse de la voiture : ' . $car->getCurrentSpeed() . ' km/h' . '<br>';
+echo $car->brake(); 
